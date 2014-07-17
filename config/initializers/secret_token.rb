13ba6e6aeb7b04f2f85c845d376ce1b5e7ca1044
laurenbenichou <laurenbenichou@gmail.com>
2014-07-16 17:36:37 -0700

    This is my first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaliforniaFoodwaysApp::Application.config.secret_token = 'd6d56513a8c59b649bb57aa33d57d4230dc9842256174e1cd1243ddeb5b92be4856feb7a6db8d5c6870212f3c5fd5c1028cd66d82af3d26816162108af31556a'
