454c2aef91829d07cfdcf6528f0927b909d46cfa
laurenbenichou <laurenbenichou@gmail.com>
2014-09-14 21:25:43 -0700

    added d3 map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,51 @@
 //= require angular-leaflet-directive.min.js
 //= require offClick
 //= require d3
+//= require topojson
+//= require queue
 //= require Modernizr
 //= require_tree ./angular
 
+$(document).ready(function(){
+  // set up margins
+  var margin = {top: 10, right: 10, bottom: 10, left: 10};
+    width = 500 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
 
+  // append svg
+  var svg = d3.select(".map").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// get json
+  queue()
+    .defer(d3.json, "assets/ca-counties.json")
+    .await(ready);
+
+  function ready(error, ca) {
+    if (error) return console.error(error);
+     console.log(ca);
+    var counties = topojson.feature(ca, ca.objects.counties);
+    var projection = d3.geo.albers()
+        .translate([width / 3, height / 3])
+        .parallels([33, 40.5])
+        .rotate([120, 0])
+        .scale(3000);
+
+    var path = d3.geo.path()
+      .projection(projection)
+
+
+    svg.selectAll(".county").data(counties.features)
+    .enter().append("path")
+    .attr("class", "county")
+    .attr("d", path)
+
+  }
+
+
+})
 
 
 
