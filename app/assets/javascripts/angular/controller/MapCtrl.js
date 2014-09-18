fabfdd601ddc6c531123a8bac5372ceb2d517ab5
laurenbenichou <laurenbenichou@gmail.com>
2014-09-17 23:23:03 -0700

    map is now fully functional

--- app/assets/javascripts/angular/controller/MapCtrl.js
+++ app/assets/javascripts/angular/controller/MapCtrl.js
@@ -2,22 +2,28 @@ app.controller('MapCtrl', ['$scope', '$http', function($scope, $http){
 
 
   $scope.locations = [];
-  $scope.locations_map = []
-  $scope.counties = []
-   var starter = "<p>Click on a county</p>"
-  // Get the data from db
+  var locations_map = []
+  var counties = []
+  var current_location = []
+  var others = []
+  var starter = "<p>Click on a county</p>"
+
+
+  // Get the present locations in database
   $http.get('/api/v1/locations.json').success(function(data) {
-      // On success, Get locations from db
+
+      // On success, Get location's name from the database
       for (i= 0; i < data.locations.length; i++){
         var county_name = data.locations[i].county
         $scope.locations.push(county_name)
       }
 
-    // d3 map
+    // create the d3 map of California
     var el = d3.select("#cali_map");
     var margin = {top: 10, right: 10, bottom: 10, left: 10};
     width = 500 - margin.left - margin.right,
     height = 550 - margin.top - margin.bottom;
+
     var projection = d3.geo.albers()
         .translate([width / 3, height / 3])
         .parallels([33, 40.5])
@@ -25,76 +31,130 @@ app.controller('MapCtrl', ['$scope', '$http', function($scope, $http){
         .scale(3000);
     var path = d3.geo.path().projection(projection)
     var svg = el.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     var g = svg.append('g');
 
 
-     queue()
-      .defer(d3.json, "assets/ca-counties.json")
-      .await(ready);
+
+    queue()
+    .defer(d3.json, "assets/ca-counties.json")
+    .await(ready);
 
     // Ready function
     function ready(error, ca) {
+      // Throws error
       if (error) return console.error(error);
+
+      // if no error then remove "loading"
       d3.select('.loading').remove();
-          debugger
-        // Get the boundaries and the labels from file
-        $scope.counties = topojson.feature(ca, ca.objects.counties).features;
-         $scope.county = g.selectAll(".county")
-                    .data($scope.counties)
+
+
+        // Get all the counties in ca from  json file
+        counties = topojson.feature(ca, ca.objects.counties).features;
+
+        // maps all the counties in california
+        $scope.county = g.selectAll(".county")
+            .data(counties)
+            .enter().append("path")
+            .attr('class', 'county')
+            .attr('d', path)
+            .on('mouseover', showCaption)
+            .on('mousemove', showCaption)
+            .on('mouseout', function() {
+             legend.html(starter);
+           });
+
+        // Compare location in db and california counties and get info
+        // for locations in the database so that we can map them
+        for (var i = 0; i < counties.length; i++) {
+        // we want to know if a[i] is found in b
+            var match = false; // we haven't found it yet
+            for (var j = 0; j < $scope.locations.length; j++) {
+              if (counties[i].properties.name == $scope.locations[j]) {
+                // we have found a[i] in b, so we can stop searching
+                match = true;
+                break;
+              }
+            // if we never find a[i] in b, the for loop will simply end,
+            // and match will remain false
+          }
+        // add a[i] to newArray only if we didn't find a match.
+        if (match) {
+          locations_map.push(counties[i]);
+        }
+      }
+
+
+        // Get all the counties from db onto map
+        $scope.county_db = g.selectAll(".county_db")
+              .data(locations_map)
+              .enter().append("path")
+              .attr('class', 'county_db')
+              .attr('d', path)
+              .on('mouseover', showCaption)
+              .on('mousemove', showCaption)
+              .on('mouseout', function() {
+               legend.html(starter);
+             })
+              .on("click", change_showStory)
+
+
+        // Function to connect the map and the select element
+        // $scope.showStory gets updated if you select a county
+        // on map and from the select option.
+
+        $scope.updateMap =  function(showStory){
+          for (var i = 0; i < locations_map.length; i++){
+            if(locations_map[i].properties.name == showStory){
+              current_location.push(locations_map[i])
+            }else{
+              others.push(locations_map[i])
+            }
+          }
+
+          // resets previously selected location
+          non_selected_locations = g.selectAll(".non_selected_location")
+                    .data(others)
                     .enter().append("path")
-                    .attr('class', 'county')
+                    .attr('class', 'non_selected_location')
                     .attr('d', path)
                     .on('mouseover', showCaption)
                     .on('mousemove', showCaption)
                     .on('mouseout', function() {
-                       legend.html(starter);
-                    });
+                     legend.html(starter);
+                   })
+                    .on("click", change_showStory)
 
-        // Compare location in db and california counties
-      for (var i = 0; i < $scope.counties.length; i++) {
-    // we want to know if a[i] is found in b
-        var match = false; // we haven't found it yet
-      for (var j = 0; j < $scope.locations.length; j++) {
-        if ($scope.counties[i].properties.name == $scope.locations[j]) {
-            // we have found a[i] in b, so we can stop searching
-            match = true;
-            break;
-        }
-        // if we never find a[i] in b, the for loop will simply end,
-        // and match will remain false
-      }
-    // add a[i] to newArray only if we didn't find a match.
-       if (match) {
-        $scope.locations_map.push($scope.counties[i]);
-      }
-    }
-
-     $scope.county_db = g.selectAll(".county_db")
-                  .data($scope.locations_map)
+          // Highlight current location on the map
+          selected_location = g.selectAll(".selected_county")
+                  .data(current_location)
                   .enter().append("path")
-                  .attr('class', 'county_db')
+                  .attr('class', 'selected_county')
                   .attr('d', path)
                   .on('mouseover', showCaption)
                   .on('mousemove', showCaption)
                   .on('mouseout', function() {
-                       legend.html(starter);
-                    })
-                  .on("click", function(){
-                    county_name = d.properties.name;
-                    $scope.locations = county_name
-                    $scope.$apply()
-                  })
+                   legend.html(starter);
+                 })
+                  .on("click", change_showStory)
 
 
-          function showCaption(d) {
-               legend = d3.select('#legend'),
-               title = d.properties.name;
-               title = title.replace("County","");
-               legend.html(title);
-             }
+        }
 
+        // Show captions in legend
+        function showCaption(d) {
+         legend = d3.select('#legend'),
+         title = d.properties.name;
+         legend.html(title);
+       }
+
+       // Change scope on click.
+       function change_showStory(d){
+        county_name = d.properties.name;
+        $scope.showStory = [county_name]
+        $scope.$apply()
+      }
     }
   })
 }])
