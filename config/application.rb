1c329f2fe9d4758e203951ea12fc446b0efc5515
laurenbenichou <laurenbenichou@gmail.com>
2014-09-21 13:50:56 -0700

    fixing templates

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ end
 
 module CaliforniaFoodwaysApp
   class Application < Rails::Application
+     config.before_initialize do |app|
+      require 'sprockets'
+      require 'angular-rails-templates'
+
+      Sprockets::Engines #force autoloading
+      Sprockets.register_engine '.html', AngularRailsTemplates::Template
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ CaliforniaFoodwaysApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => 'californiafoodways.com' }
+   config.action_mailer.default_url_options = { :host => 'californiafoodways.com' }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
