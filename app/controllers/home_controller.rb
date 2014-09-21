713a94ab98f9130cc6714f5e6371ea029e0c6b27
laurenbenichou <laurenbenichou@gmail.com>
2014-09-21 02:56:10 -0700

    trying to fix templates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-     render layout: layout_name
+     # render layout: layout_name
   end
 
-  def layout_name
-    if params[:layout] == 0
-        false
-    else
-        'application'
-    end
-  end
+  # def layout_name
+  #   if params[:layout] == 0
+  #       false
+  #   else
+  #       'application'
+  #   end
+  # end
 
 end
