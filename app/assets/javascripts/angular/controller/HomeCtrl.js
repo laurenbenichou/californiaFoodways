57bab5e807816eab82203ee164e7b12e021b6be5
laurenbenichou <laurenbenichou@gmail.com>
2014-09-21 02:47:38 -0700

    commented out google analytics

--- app/assets/javascripts/angular/controller/HomeCtrl.js
+++ app/assets/javascripts/angular/controller/HomeCtrl.js
@@ -1,10 +1,11 @@
 app.controller('HomeCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
 
-
     $scope.stories = [];
     // Get stories from datavase
     $http.get('/api/v1/stories.json').success(function(data) {
+      console.log(data)
       return $scope.stories = data.stories;
+
     });
 
     // Get soundcloud Iframe
