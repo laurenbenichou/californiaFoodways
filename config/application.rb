9181a14a80f665931ff15a929931534a3bd2f8a8
laurenbenichou <laurenbenichou@gmail.com>
2014-09-21 00:47:58 -0700

    removed line in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module CaliforniaFoodwaysApp
   class Application < Rails::Application
-     require "#{Rails.root}/config/initializers/bower_rails.rb"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
