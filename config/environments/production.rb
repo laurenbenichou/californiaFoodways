cabd1afc9abd8c8b79dfeda3da033698c71716ca
laurenbenichou <laurenbenichou@gmail.com>
2014-09-20 22:30:52 -0700

    added host in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ CaliforniaFoodwaysApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+   config.action_mailer.default_url_options = { :host => 'http://californiafoodways.herokuapp.com/' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
